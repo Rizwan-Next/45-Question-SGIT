6ca99e8a2832cad55f3d3b05f045549d0617b03d
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-10 14:59:13 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,87 @@
+//Q 2
+//  Personal Message: Store a person’s name in a variable,
+//  and print a onmessage to that person. Your message should be simple,
+//  such as, “Hello Eric, would you like to learn some Python today?”
+
+// let personName:string = "Rizwan Siddique";
+// console.log(`Hello ${personName} would you like to learn some pythan today?`);
+// ________________________________________
+
+// Question 3 lowerCase
+
+// let personName: string = "karachi pakistan";
+// console.log("lowerCase:", personName.toLowerCase());
+
+// console.log("UpperCase:", personName.toLocaleUpperCase());
+
+// console.log("TitleCase:", personName.replace(/\bw/g,c => c.toUpperCase()));
+// ________________________________________
+
+// Question 4 
+
+// let qoute: string = "A person who never made a mistake never trid anything new";
+// let Author: string = "Albert Einstein";
+// console.log(`${Author} Once said, ${qoute}`);
+// _________________________________________
+
+// Question No. 5
+
+/*let quote: string = "A person who never made a mistake never trid anything new";
+let famous_person: string = "Albert Einstain";
+let message = `${famous_person} Once said, ${quote}`;
+console.log(message);*/
+// ______________________________________________
+
+//Question 6
+//let personName: String = `\n\t Rizwan Siddique \t\n`;
+//console.log(personName);
+//let stripped: string = personName.trim();
+//console.log(stripped);
+//___________________________________________________
+
+//Question No. 7 & 8
+
+// console.log (2+6);
+// console.log (9-1);
+//  console.log (24/3);
+//  console.log (4*2);
+// ____________________________________________ 
+
+// Question No 9
+
+// let faveritnumber: number = 10;
+// console.log(`My Faverit Numer is ${faveritnumber}`);
+// ___________________________________
+
+// Question No. 10
+
+//  Rizwan
+// // 01/05/2024
+// console.log(`A B C D E`);
+// console.log(4*5);
+//_____________________________
+
+// Question No.11
+
+// let friends:string[] = [`saleem, Zeeshan, Raheem`];
+// for(let i=0; i<friends.length; i++){
+//     console.log(friends[i]);
+
+// }
+// __________________________________
+
+// question no. 12
+
+// let friends: string[] = [`Saleem`, `Zeeshan`, `Raheem`];
+// let massage: String =`you are invited for a dinner today  `;
+//  for(let i=0; i<friends.length; i++){
+//     console.log(massage + friends[i]);
+// }
+// __________________________________________________
+
+// Question No. 13
+
+// let tranportation: string [] = [`Civic`,`Bick`, `Bus`,`Suzuki`];
+// for (let i=0; i<tranportation.length; i++){
+//     console.log(`I would like to own a `+ tranportation[i]);
+// }
