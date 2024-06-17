c5db97e7af0b3742e1c5b483f732bed1d82605ef
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-17 15:16:16 +0500

    Add files via upload

---
+++ Questions 1 to 13/Q 13/app.ts
@@ -0,0 +1,6 @@
+// Question No. 13
+
+let tranportation: string [] = [`Civic`,`Bick`, `Bus`,`Suzuki`];
+for (let i=0; i<tranportation.length; i++){
+    console.log(`I would like to own a `+ tranportation[i]);
+}
