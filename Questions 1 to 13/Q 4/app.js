91401ed6d8af3aa89139b28870cc4d9a3c16510e
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-17 15:01:38 +0500

    Add files via upload

---
+++ Questions 1 to 13/Q 4/app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question 4 
+let qoute = "A person who never made a mistake never trid anything new";
+let Author = "Albert Einstein";
+console.log(`${Author} Once said, ${qoute}`);
---
+++ Questions 1 to 13/Q 4/app.ts
@@ -0,0 +1,6 @@
+
+// Question 4 
+
+let qoute: string = "A person who never made a mistake never trid anything new";
+let Author: string = "Albert Einstein";
+console.log(`${Author} Once said, ${qoute}`);
