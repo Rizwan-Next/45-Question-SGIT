cf0f9429888d7bdeaae9126714f6f4319cf1601e
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-17 15:02:42 +0500

    Add files via upload

---
+++ Questions 1 to 13/Q 5./app.ts
@@ -0,0 +1,7 @@
+
+// Question No. 5
+
+let quote: string = "A person who never made a mistake never trid anything new";
+let famous_person: string = "Albert Einstain";
+let message = `${famous_person} Once said, ${quote}`;
+console.log(message);
