a12f41b49ad99dbf2223ab47c2023d0b794d29b2
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-17 15:09:07 +0500

    Add files via upload

---
+++ Questions 1 to 13/Q 10/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question No. 10
+//  Rizwan
+// 01/05/2024
+console.log(`A B C D E`);
+console.log(4 * 5);
---
+++ Questions 1 to 13/Q 10/app.ts
@@ -0,0 +1,7 @@
+// Question No. 10
+
+//  Rizwan
+// 01/05/2024
+
+console.log(`A B C D E`);
+console.log(4*5);
