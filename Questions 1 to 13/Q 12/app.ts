3d7b5ad0304483b2e49f0a2607ac7af98d417463
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-17 15:12:44 +0500

    Add files via upload

---
+++ Questions 1 to 13/Q 12/app.ts
@@ -0,0 +1,7 @@
+// question no. 12
+
+let friends: string[] = [`Saleem`, `Zeeshan`, `Raheem`];
+let massage: String =`you are invited for a dinner today  `;
+ for(let i=0; i<friends.length; i++){
+    console.log(massage + friends[i]);
+}
