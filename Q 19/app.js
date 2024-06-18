b555cbfd822434bbefd5a1cf350042fee0ad95c6
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-18 11:11:43 +0500

    Add files via upload

---
+++ Q 19/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("../Q 14 to Q 17/app");
+console.log(`n\ Printing Guest List Invited `);
+console.log(`We had finally invited ${app_1.guest_list.length} from Question No. 14`);
---
+++ Q 19/app.ts
@@ -0,0 +1,3 @@
+import{ guest_list } from '../Q 14 to Q 17/app'
+console.log(`n\ Printing Guest List Invited `)
+console.log(`We had finally invited ${guest_list.length} from Question No. 14`)
