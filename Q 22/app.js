53219dfe1208fd9d4c967de65fa09fb5671923d4
Rizwan Ali Shaikh <168621049+Rizwan-Next@users.noreply.github.com>
2024-06-18 11:15:46 +0500

    Add files via upload

---
+++ Q 22/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let array = ["Tahir Khan", "Noman Ashrfi", 1, 2, 3, "Minahil"];
+console.log(array[3]);
+console.log(array[2]);
+console.log(array[5]);
+console.log(array[0]);
+console.log(array[4]);
+console.log(array[1]);
